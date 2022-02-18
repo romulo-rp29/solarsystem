11b3218ec652b82966adffadb128bc33af8ce906
romulo-rp29 <romul.ceuma@gmail.com>
2022-02-18 19:56:57 -0300

    completed req 6

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
+import Planets from '../data/planets';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {
+          Planets.map((planet) => (<PlanetCard
+            planetName={ planet.name }
+            planetImage={ planet.image }
+            key={ planet.name }
+          />))
+        }
       </div>
     );
   }
