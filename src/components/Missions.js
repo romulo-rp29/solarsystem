88e9f5fd238c2944503b45c65c76e40f93a652fa
romulo-rp29 <romul.ceuma@gmail.com>
2022-02-18 20:59:11 -0300

    completed req 10

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends React.Component {
   render() {
     return (
-      <Title headline="Missões" />
+      <div data-testid="missions">
+        <Title headline="Missões" />
+        {
+          missions.map((mission) => (<MissionCard
+            name={ mission.name }
+            year={ mission.year }
+            country={ mission.country }
+            destination={ mission.destination }
+            key={ mission.name }
+          />))
+        }
+      </div>
     );
   }
 }
