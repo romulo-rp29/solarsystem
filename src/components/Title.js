e773b968c14efabfe6d0625e0dde9f99c20b8ff9
romulo-rp29 <romul.ceuma@gmail.com>
2022-02-18 16:05:57 -0300

    update req 3 req 4

---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Title extends React.Component {
+  render() {
+    const headline = 'Planetas';
+    return (
+      <h2>{headline}</h2>
+    );
+  }
+}
+
+export default Title;
